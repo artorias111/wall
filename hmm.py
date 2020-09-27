da3eeb8d5bab293e07546d718796d85033e7c74c
Shriram Bhat <48955393+artorias111@users.noreply.github.com>
2020-09-27 09:03:59 +0530

    Update hmm.py

--- hmm.py
+++ hmm.py
@@ -1,6 +1,4 @@
-#https://www.pexels.com/search/desktop%20wallpaper/
-
-#pexels api key: 563492ad6f917000010000013b7de554cc354ac6b1ea6f2c419369e3
+#https://source.unsplash.com/daily
 
 
 import requests as re
@@ -8,8 +6,4 @@ from bs4 import BeautifulSoup as bs
 from os import makedirs
 from time import sleep
 
-url=r'https://www.pexels.com/search/desktop%20wallpaper/'
-
-
-else:
-    print("The pexels website is down right now, please try later")
+url=r'https://source.unsplash.com/daily'
